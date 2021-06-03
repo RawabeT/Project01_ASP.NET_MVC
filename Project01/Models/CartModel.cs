3293960ed32c9005be6074edc30f44a2e8415003
rawabe4 <rawabe44u@gmail.com>
2021-06-03 14:00:19 +0300

    add appContext

---
+++ Project01/Models/CartModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project01.Models
+{
+    public class CartModel
+    {
+        public int Id { get; set; }
+        public string Image { get; set; }
+        public int TotalPrice { get; set; }
+        public string ItemName { get; set; }
+        public int Quantity { get; set; }
+
+    }
+}
---
+++ Project01/Models/OrderModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project01.Models
+{
+    public class OrderModel
+    {
+        public int Id { get; set; }
+        public int TotalPrice { get; set; }
+        public int Quantity { get; set; }
+        public DateTime OrderDate { get; set; }
+        //public int ItemId { get; set; }
+    }
+}
